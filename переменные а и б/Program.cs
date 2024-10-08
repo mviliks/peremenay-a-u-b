a9929c0e092b6ac08f07d294096c517d58120e91
1808v <1808v@MVILIKS>
2024-10-08 13:14:59 +0300

    Добавьте файлы проекта.

---
+++ переменные а и б/Program.cs
@@ -0,0 +1,34 @@
+﻿//Михайлова Виктория. ИСП-232о. Задание:переменные
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace переменные_а_и_б
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Онлайн калькулятор");
+            Console.Write("Введите значение a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите переменную с: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double sum = a + b;
+            double difference = a - b;
+            double product = a * b;
+            double summ = (a + b) * c;
+            Console.WriteLine($"Сумма a и b: {sum}");
+            Console.WriteLine($"Разность a и b: {difference}");
+            Console.WriteLine($"Произведение a и b: {product}");
+            Console.WriteLine($"Выражение (а+b)*c: {summ}");
+            Console.ReadLine();
+        }
+    }
+}
